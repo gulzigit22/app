927e13c0ff053c7e1274544c7c83487a8f7c623d
gulzigit22 <nabievv02@gmail.com>
2024-04-25 13:06:47 +0600

    ssadsd

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amberAccent,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
